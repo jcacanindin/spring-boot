c5eed9757cfffcc4a39eed85ead33cc0a9264d7f
Patrick Villanueva <plvillanueva@infobuilder.ph>
2022-10-27 21:37:32 +0800

    adde request path and mapping per method, also corrected request types

--- src/main/java/ph/homecredit/exam/controller/PersonController.java
+++ src/main/java/ph/homecredit/exam/controller/PersonController.java
@@ -1,13 +1,30 @@
 package ph.homecredit.exam.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ph.homecredit.exam.entity.Person;
+import ph.homecredit.exam.exception.PersonException;
 
+@RequestMapping("/default")
 public interface PersonController {
+    
+    @GetMapping("/persons")
+    Person getPerson(Long id);
 
-    Person createPerson(Person person);
+    @PostMapping("/persons")
+    Person createPerson(Person person) throws PersonException;
 
-    Person updatePerson(Long id, Person person);
+    @PutMapping("/persons")
+    Person updatePerson(@RequestParam Long id, Person person);
 
-    Long deletePerson(Long id);
-
+    @DeleteMapping("/persons/{id}")
+    Long deletePerson(@PathVariable("id") Long id);
+    
+    @GetMapping("/test")
+    String test();
 }
--- src/main/java/ph/homecredit/exam/controller/PersonControllerImpl.java
+++ src/main/java/ph/homecredit/exam/controller/PersonControllerImpl.java
@@ -1,32 +1,46 @@
 package ph.homecredit.exam.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ph.homecredit.exam.entity.Person;
+import ph.homecredit.exam.exception.PersonException;
 import ph.homecredit.exam.service.PersonService;
 
+@RestController
+@RequestMapping("/homecredit/test")
 public class PersonControllerImpl implements PersonController {
 
-    @Autowired
-    private PersonService personService;
+    private final PersonService personService;
+    
+    public PersonControllerImpl(PersonService personService){
+        this.personService = personService;
+    }
 
-    @PostMapping("/persons")
     @Override
-    public Person createPerson(Person person) {
+    public Person createPerson(@RequestBody Person person) throws PersonException {
         return personService.createPerson(person);
     }
 
-    @PostMapping("/persons")
     @Override
-    public Person updatePerson(@PathVariable("id") Long id, Person person) {
+    public Person updatePerson(@PathVariable("id") Long id, @RequestBody Person person) {
         return personService.updatePerson(id, person);
     }
 
-    @PostMapping("/persons")
     @Override
     public Long deletePerson(Long id) {
         return personService.deletePerson(id);
+    }
+    
+    @Override
+    public String test(){
+        return "success";
+    }
+
+    @Override
+    public Person getPerson(Long id) {
+       return personService.getPerson(id);
     }
 
 }
