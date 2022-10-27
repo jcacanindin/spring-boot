865c11e5c9686477792a9f6df2b3422a6fd50504
Patrick Villanueva <plvillanueva@infobuilder.ph>
2022-10-27 21:40:07 +0800

    implemented create, update, and delete operations, also added read operation

--- src/main/java/ph/homecredit/exam/service/PersonService.java
+++ src/main/java/ph/homecredit/exam/service/PersonService.java
@@ -1,13 +1,16 @@
 package ph.homecredit.exam.service;
 
 import ph.homecredit.exam.entity.Person;
+import ph.homecredit.exam.exception.PersonException;
 
 public interface PersonService {
 
-    Person createPerson(Person person);
+    Person createPerson(Person person) throws PersonException;
 
     Person updatePerson(Long id, Person person);
 
     Long deletePerson(Long id);
+    
+    Person getPerson(Long id);
 
 }
--- src/main/java/ph/homecredit/exam/service/PersonServiceImpl.java
+++ src/main/java/ph/homecredit/exam/service/PersonServiceImpl.java
@@ -1,8 +1,19 @@
 package ph.homecredit.exam.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ph.homecredit.exam.entity.Person;
+import ph.homecredit.exam.exception.PersonException;
+import ph.homecredit.exam.repository.PersonRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
 public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    PersonRepository personRepository;
 
     /**
      * This should insert to an h2 database
@@ -11,7 +22,25 @@ public class PersonServiceImpl implements PersonService {
      * @return created person
      */
     @Override
-    public Person createPerson(Person person) {
+    public Person createPerson(Person person) throws PersonException {
+        String alphabetRegex = "^[a-zA-Z]*$";
+        
+        if(person.getFirstName() == null || person.getLastName() == null || person.getAge() == null || person.getEmail() == null)
+            throw new PersonException("Person contains a null value, please try again.");
+
+        if(!person.getFirstName().matches(alphabetRegex) || !person.getLastName().matches(alphabetRegex))
+            throw new PersonException("Person's names should only contain alphabets, please try again.");
+        
+        if(person.getMiddleName() != null && !person.getMiddleName().matches(alphabetRegex))
+            throw new PersonException("Person's names should only contain alphabets, please try again.");
+
+        if(!person.getEmail().matches("^[A-Za-z0-9+_.-]+@(.+)$"))
+            throw new PersonException("Person's email is not valid, please try again.");
+
+        person.setCreated_Date(LocalDateTime.now());
+        person.setLast_Modified_Date(LocalDateTime.now());
+        personRepository.save(person);
+
         return person;
     }
 
@@ -24,6 +53,29 @@ public class PersonServiceImpl implements PersonService {
      */
     @Override
     public Person updatePerson(Long id, Person person) {
+        Optional<Person> personFound = personRepository.findById(id);
+        person.setLast_Modified_Date(LocalDateTime.now());
+        
+        if(personFound.isPresent()){
+            Person newPerson = personFound.get();
+            LocalDateTime createdDate = newPerson.getCreated_Date();
+            
+            person.setId(newPerson.getId());
+            person.setCreated_Date(createdDate);
+            
+            newPerson.setId(person.getId());
+            newPerson.setFirstName(person.getFirstName());
+            newPerson.setMiddleName(person.getMiddleName());
+            newPerson.setLastName(person.getLastName());
+            newPerson.setAge(person.getAge());
+            newPerson.setEmail(person.getEmail());
+            newPerson.setCreated_Date(person.getCreated_Date());
+            newPerson.setLast_Modified_Date(person.getLast_Modified_Date());
+            
+            personRepository.save(newPerson);
+        }
+            
+        
         return person;
     }
 
@@ -33,7 +85,17 @@ public class PersonServiceImpl implements PersonService {
      */
     @Override
     public Long deletePerson(Long id) {
-        return null;
+        Optional<Person> personToBeDeleted = personRepository.findById(id);
+        
+        if(personToBeDeleted.isPresent())
+            personRepository.deleteById(id);
+        
+        return id;
+    }
+
+    @Override
+    public Person getPerson(Long id) {
+        return personRepository.findById(id).orElse(null);
     }
 
 }
